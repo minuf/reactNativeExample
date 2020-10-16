7774e39d48fe909c7199225ea7a0aab37cd4890d
jorge-msi <minuel.jc@gmail.com>
2020-10-16 12:34:37 +0200

    Moved index.js and App.js to src folder, modified MainApplication on android for read this change

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import React from 'react';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {name as appName} from '../app.json';
 
 import { Provider } from 'react-redux';
-import { store } from './src/_helpers';
+import { store } from './_helpers';
 
 function Store() {
     return (
