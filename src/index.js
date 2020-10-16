211ebe2c1cda30eb4f9a2c08e792c6c9327a832b
jorge-msi <minuel.jc@gmail.com>
2020-10-16 12:50:34 +0200

    Moved entry point index.js to src folder and App.js app.json to src/app folder

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
 import React from 'react';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from '../app.json';
+import App from './app/App';
+import {name as appName} from './app/app.json';
 
 import { Provider } from 'react-redux';
 import { store } from './_helpers';
